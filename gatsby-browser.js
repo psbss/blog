1ca417f8611eacecd795cf72e45c7eba00956fc5
uetyo <uehara.yuki@dely.jp>
2024-11-03 03:34:50 +0900

    fix use react package

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export const wrapRootElement = ({ element }) => (
   <>
