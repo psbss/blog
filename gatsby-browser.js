da018fb9443d3b4cce92774fb90ea9255b968355
uetyo <uypsbss@gmail.com>
2024-11-03 03:36:11 +0900

    [Infra] add vercel/speed-insights (#151)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export const wrapRootElement = ({ element }) => (
   <>
     {element}
     <Analytics />
+    <SpeedInsights />
   </>
 );
